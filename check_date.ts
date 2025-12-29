b62a39a1999724bee32f2846cf5308b38da8fc0d
Ovie Okeh <kevinokeh@gmail.com>
2025-12-29 10:01:00 +0100

    cp

--- check_date.ts
+++
@@ -1,7 +0,0 @@
-const start = new Date("2025-06-01");
-const end = new Date("2025-06-03");
-const dayCount = Math.max(1, Math.ceil((end.getTime() - start.getTime()) / 86400000) + 1);
-console.log("Start:", start);
-console.log("End:", end);
-console.log("Diff:", end.getTime() - start.getTime());
-console.log("DayCount:", dayCount);
